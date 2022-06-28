6e82d76d0f7616e16b18e61d1629dd25997addf2
vandhana516 <vandhu0813@gmail.com>
2022-06-28 22:09:28 +0530

    First Time Push

---
+++ src/test/java/projectclassGit/Gitclass.java
@@ -0,0 +1,20 @@
+package projectclassGit;
+
+public class Gitclass {
+private void method1() {
+	System.out.println("1");
+	
+}
+private void method2() {
+	System.out.println("2");
+	
+}
+	private void method3() {
+		System.out.println("3");
+	}
+	private void method4() {
+		System.out.println("4");
+	}
+	
+	
+}
